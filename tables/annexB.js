57d4061de62a36982174b982fe33f70eaf7de045
SPLP-2023 <Luke.storey@strikepoint.uk>
2025-08-05 11:30:45 +0100

    Add files via upload

---
+++ tables/annexB.js
@@ -0,0 +1,11 @@
+// tables/annexB.js
+export function calcPA(PTA, PB) { return PTA * PB; }
+export function calcPB(PB) { return PB; }
+export function calcPC(PSPD) { return PSPD; }
+export function calcPM(PM) { return PM; }
+export function calcPU(PEB) { return PEB; }
+export function calcPV(PEB) { return PEB; }
+export function calcPW(PSPD) { return PSPD; }
+export function calcPLI(PLI) { return PLI; }
+export function calcPZ(PLI) { return PLI; }
+
